d3dd53288415a4c4aa022a5ddea5ef025ea98ee2
Jeterson Miranda Gomes <jmg@cin.ufpe.br>
2022-01-12 14:12:22 -0300

    Backend created

---
+++ backend/src/main/java/com/jeterson/ratemovie/RateMovieApplication.java
@@ -0,0 +1,13 @@
+package com.jeterson.ratemovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RateMovieApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/jeterson/ratemovie/RateMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeterson.ratemovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RateMovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
