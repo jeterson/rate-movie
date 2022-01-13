062342b0f7e270fe3619638c4583a9fb3679a840
Jeterson Miranda Gomes <jmg@cin.ufpe.br>
2022-01-13 12:06:40 -0300

    MovieCard

---
+++ frontend/src/components/MovieCard/index.tsx
@@ -0,0 +1,53 @@
+import { ReactComponent as StarFull } from 'assets/images/star-full.svg'
+import { ReactComponent as StarHalf } from 'assets/images/star-half.svg'
+import { ReactComponent as StarEmpty } from 'assets/images/star-empty.svg'
+import { HtmlHTMLAttributes } from 'react'
+import './style.css'
+const movie = {
+  id: 1,
+  image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
+  title: "The Witcher",
+  count: 2,
+  score: 4.5
+};
+
+export type MovieCardProps = HtmlHTMLAttributes<HTMLElement> & {}
+
+function MovieStars() {
+  return (
+    <div className="j-movie-stars-container">
+      <StarFull />
+      <StarFull />
+      <StarFull />
+      <StarHalf />
+      <StarEmpty />
+    </div>
+  )
+}
+
+export default function MovieCard(props: MovieCardProps) {
+  return (
+    <div {...props} className="j-movie-card-container" >
+      <div className="j-movie-card-box">
+        <div className="j-movie-card-image-container">
+          <img src={movie.image} />
+        </div>
+        <div className="j-movie-card-title">
+          <span>{movie.title}</span>
+        </div>
+        <div className="j-movie-card-score">
+          <span>{movie.score}</span>
+        </div>
+        <div className="j-movie-card-stars">
+          <MovieStars />
+          <span>{`${movie.count} ${movie.count == 1 ? ' avaliação' : ' avaliações'}`}</span>
+        </div>
+
+        <div className="j-movie-card-action">
+          <button>Avaliar</button>
+        </div>
+
+      </div>
+    </div>
+  )
+}
--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,23 @@
+import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
-
+import './style.css'
 export default function Listing() {
   return (
-    <Pagination />
+    <div className="j-listing-page">
+      <div className="j-listing-pagination-container">
+        <Pagination />
+      </div>
+      <div className="j-listing-page-container">
+        <MovieCard className="j-movie-card-item" />
+        <MovieCard className="j-movie-card-item" />
+        <MovieCard className="j-movie-card-item" />
+        <MovieCard className="j-movie-card-item" />
+        <MovieCard className="j-movie-card-item" />
+        <MovieCard className="j-movie-card-item" />
+        <MovieCard className="j-movie-card-item" />
+        
+        
+      </div>
+    </div>
   )
 }
