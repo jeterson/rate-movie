3f356b0650ecd9fba93a59846642613cdae50728
Jeterson Miranda Gomes <jmg@cin.ufpe.br>
2022-01-14 18:00:12 -0300

    React Hooks

--- frontend/src/components/Pagination/index.tsx
+++ frontend/src/components/Pagination/index.tsx
@@ -1,18 +1,28 @@
 import './style.css'
 import {ReactComponent as PrevIcon} from 'assets/images/prev-icon.svg'
 import {ReactComponent as NextIcon} from 'assets/images/next-icon.svg'
+import { MoviePage } from 'types/movie'
+import { useState } from 'react'
 
+export type PaginationProps = {
+  page: MoviePage|undefined
+  onNext: () => void
+  onPrev: () => void
+}
 
+export default function Pagination(props: PaginationProps) { 
 
-export default function Pagination() {
+  const handleTeste = () => {
+    console.log("Teste")
+  }
   return (
     <div className="j-pagination-container">
       <div className="j-pagination-buttons">
-        <button disabled className="j-pagination-btn j-btn-prev">
+        <button onClick={handleTeste} disabled className="j-pagination-btn j-btn-prev">
           <PrevIcon />
         </button>
-        <span className="j-pagination-info">{`${1} de ${3}`}</span>
-        <button className="j-pagination-btn btn-next">
+        <span className="j-pagination-info">{`${(props.page?.number??0)+ 1} de ${props.page?.totalPages??0}`}</span>
+        <button onClick={() => props.onPrev} className="j-pagination-btn btn-next">
           <NextIcon />
         </button>
       </div>
--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,19 +1,33 @@
 import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { useEffect, useState } from "react";
+import { MoviePage } from "types/movie";
 import { BASE_URL } from "utils/request";
 import './style.css'
-export default function Listing() {
+export default function Listing() {  
+  const [page, setPage] = useState<MoviePage|undefined>()
+  const [pageNumber, setPageNumber] = useState(0)
 
-  axios.get(`${BASE_URL}/movies`)
-  .then(res => {
-    console.log(res.data)
-  })
+  useEffect(() => {
+    axios.get(`${BASE_URL}/movies?pageNumber${pageNumber}`)
+    .then(res => {
+      setPage(res.data as MoviePage)
+    })
+  }, [pageNumber])
+
+  const handleNextPage = () => {
+    setPageNumber(pageNumber+1)    
+    console.log(pageNumber)
+  }
+  const handlePrevPage = () => {
+    setPageNumber(pageNumber-1)
+  }
 
   return (
     <div className="j-listing-page">
       <div className="j-listing-pagination-container">
-        <Pagination />
+        <Pagination onNext={() => handleNextPage()} onPrev={() => handlePrevPage()} page={page} />
       </div>
       <div className="j-listing-page-container">
         <MovieCard className="j-movie-card-item" />
