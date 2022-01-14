105a672eec00aef84c0441c02a4ea9584892ac65
Jeterson Miranda Gomes <jmg@cin.ufpe.br>
2022-01-14 17:08:44 -0300

    First Requesty

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,15 @@
+import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { BASE_URL } from "utils/request";
 import './style.css'
 export default function Listing() {
+
+  axios.get(`${BASE_URL}/movies`)
+  .then(res => {
+    console.log(res.data)
+  })
+
   return (
     <div className="j-listing-page">
       <div className="j-listing-pagination-container">
