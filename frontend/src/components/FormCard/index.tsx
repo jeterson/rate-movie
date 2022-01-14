6a48b9bec4180de9cb49514b29d492fd65baeb19
Jeterson Miranda Gomes <jmg@cin.ufpe.br>
2022-01-14 20:44:53 -0300

    Paginatio

--- frontend/src/components/FormCard/index.tsx
+++ frontend/src/components/FormCard/index.tsx
@@ -2,26 +2,50 @@ import './style.css'
 import { useNavigate } from 'react-router-dom'
 import { FormEvent, useState } from 'react';
 import { Movie } from 'types/movie';
+import axios from 'axios';
+import { BASE_URL } from 'utils/request';
+import { validateEmail } from 'utils/validations';
+
 
 
 type FormCardProps = {
   movie?: Movie
 }
 
+type ScoreForm = {
+  email: string,
+  score: number
+}
 export default function FormCard({ movie }: FormCardProps) {
-  const navigate = useNavigate();  
+  const navigate = useNavigate();
 
-  const handleSubmit = (e: FormEvent) => {
-    e.preventDefault()
-    navigate('/')
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const target = e.target as typeof e.target & {
+      email: { value: string };
+      score: { value: number };
+    };
+    const email = target.email.value
+    const score = target.score.value
+
+    const payload = {
+      movieId: movie?.id,
+      email: email,
+      score: score
+    }
+    console.log(payload)
+    axios.put(`${BASE_URL}/scores`, payload)
+      .then(res => {        
+        navigate('/')
+      })
   }
   return (
     <div className="j-page">
       <div className="j-card">
         <div className="j-image-container">
           {
-          !movie ? <span className="j-image-loading">Carregando</span> :
-          <img alt={movie?.title} className="j-image" src={movie?.image} />
+            !movie ? <span className="j-image-loading">Carregando</span> :
+              <img alt={movie?.title} className="j-image" src={movie?.image} />
           }
         </div>
         <div className="j-form-title">{movie?.title}</div>
@@ -29,17 +53,17 @@ export default function FormCard({ movie }: FormCardProps) {
 
           <div className="j-input-container">
             <label htmlFor="email">Informe seu email</label>
-            <input id="email" />
+            <input required type="email" name="email" id="email" />
           </div>
           <div className="j-spacing" />
           <div className="j-input-container">
             <label htmlFor="email">Informe sua avaliação</label>
-            <select id="email" className="j-rating">
-              <option style={{ height: '100px' }}>1</option>
-              <option>2</option>
-              <option>3</option>
-              <option>4</option>
-              <option>5</option>
+            <select required name="score" id="score" className="j-rating">
+              <option value={1} style={{ height: '100px' }}>1</option>
+              <option value={2}>2</option>
+              <option value={3}>3</option>
+              <option value={4}>4</option>
+              <option value={5}>5</option>
             </select>
           </div>
           <div className="j-spacing-lg" />
